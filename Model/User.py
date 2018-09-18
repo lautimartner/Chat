737b9145c0414c1dfbf030ced613460c69995119
lautimartner <lautimartner@ciencias.unam.mx>
2018-09-18 13:35:17 -0500

    Agregue las clases User y Chatroom

--- Model/User.py
+++ Model/User.py
@@ -1,8 +1,24 @@
+import random
 class User:
-    def __init__(self, conn_socket, name, status, unique_number):
-        self.conn_socket = conn_socket
+    def __init__(self, name, status, client_socket):
         self.name = name
         self.status = status
-        self.unique_number = unique_number
+        self.client_socket = client_socket
 
+    def getName(self):
+        return self.name
     
+    def getStatus(self):
+        return self.status
+    
+    def getUniqueNumber(self):
+        return self.unique_number
+    
+    def setName(self,nname):
+        self.name=nname
+    
+    def setStatus(self,sstatus):
+        self.status=sstatus
+
+    def getClientSocket(self):
+        return self.client_socket
